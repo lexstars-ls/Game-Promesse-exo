5f835c21af2b298c3b0c7356412a4b81bd3aff06
Sisovath lucas <lucas.sisovath@egs.school>
2024-04-23 14:55:03 +0200

    correctif promesse et html

--- js/header.js
+++ js/header.js
@@ -9,60 +9,54 @@ export function header() {
         }
     });
 
-    headerPromise.then(function (games) {
-        return games.length;
-    })
-        .then(function (gameLength) {
-            console.log(` Il y a ${gameLength} jeux`);
+    headerPromise
+        .then(function (games) {
+            return games
+        })
+        .then(function (games) {
+            const body = document.querySelector('body');
+            const templateHtml = document.querySelector('#gameTemplate');
+            let cloneTemplate = document.importNode(templateHtml.content, true);
+            let title = cloneTemplate.querySelector('h1');
+            let sectionTitle = cloneTemplate.querySelector('h3');
+            let lastSectionTitle = cloneTemplate.querySelector('h4');
+
+            title.textContent = "Jeux Vidéo EGS l'actu au quotidien";
+            sectionTitle.textContent = "Les derniers jeux notés";
+            lastSectionTitle.textContent = "Les dernières news";
+
+            const headerSection = cloneTemplate.querySelector('section');
+
+            // Parcourir la liste des jeux
+            for (let i = 0; i < games.length; i++) {
+                const gameDiv = cloneTemplate.querySelector('div');
+                const gameImage = cloneTemplate.querySelector('img');
+                const gameName = cloneTemplate.querySelector('h2');
+                const gameRate = cloneTemplate.querySelector('.etoile');
+
+                // Assigner les valeurs
+                gameImage.src = games[i].image;
+                gameImage.alt = games[i].name;
+                gameName.textContent = games[i].name;
+
+                // rating de 0/5 avec img par def
+                for (let j = 0; j < 5; j++) {
+                    const star = document.createElement('img');
+                    star.src = j < games[i].rate ? "./img/star.png" : "./img/starEmpty.png";
+                    gameRate.appendChild(star);
+                }
+
+                // Ajouter à la div principale puis à header
+                gameDiv.appendChild(gameImage);
+                gameDiv.appendChild(gameName);
+                gameDiv.appendChild(gameRate);
+
+                headerSection.appendChild(gameDiv);
+            }
+
+            templateHtml.appendChild(cloneTemplate);
         })
         .catch(function (error) {
             console.error(error);
         });
-
-
-    const body = document.querySelector('body');
-    const templateHtml = document.querySelector('#header-template');
-    let cloneTemplate = document.importNode(templateHtml.content, true);
-    let title = cloneTemplate.querySelector('h1');
-    let sectionTitle = cloneTemplate.querySelector('h3');
-    let lastSectionTitle = cloneTemplate.querySelector('h4');
-
-
-    title.textContent = "Jeux Vidéo EGS l'actu au quotidien";
-    sectionTitle.textContent = "Les derniers jeux notés";
-    lastSectionTitle.textContent = "Les dernières news";
-
-    const headerSection = cloneTemplate.querySelector('section');
-
-    // Parcourir la liste des jeux
-    for (let i = 0; i < GAMES.length; i++) {
-
-        const gameDiv = cloneTemplate.querySelector('div');
-        const gameImage = cloneTemplate.querySelector('img');
-        const gameName = cloneTemplate.querySelector('h2');
-        const gameRate = cloneTemplate.querySelector('.etoile');
-
-        // Assigner les valeurs
-        gameImage.src = GAMES[i].image;
-        gameImage.alt = GAMES[i].name;
-        gameName.textContent = GAMES[i].name;
-
-        // rating de 0/5 aveci mg par def
-        for (let j = 0; j < 5; j++) {
-            const star = document.createElement('img');
-            star.src = j < GAMES[i].rate ? "./img/star.png" : "./img/starEmpty.png";
-            gameRate.appendChild(star);
-        }
-
-        // Ajouter a la div principal puis a header
-        gameDiv.appendChild(gameImage);
-        gameDiv.appendChild(gameName);
-        gameDiv.appendChild(gameRate)
-
-
-        headerSection.appendChild(gameDiv);
-
-    }
-
-    body.appendChild(cloneTemplate);
 }
